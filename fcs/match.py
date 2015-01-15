4b4a1d6994968cf019819b9987ca0887e2be6173
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2015-01-15 15:33:48 +0200

    catch and log connection error if callback to BDR fails

--- fcs/match.py
+++ fcs/match.py
@@ -43,16 +43,20 @@ def do_bdr_request(params):
     url = get_absolute_url('/ReportekEngine/update_company_collection')
     auth = get_auth()
     ssl_verify = current_app.config['HTTPS_VERIFY']
-    response = requests.get(url, params=params, auth=auth, verify=ssl_verify)
 
     error_message = ''
-    if (response.status_code == 200 and
-       response.headers.get('content_type') == 'application/json'):
-        json_data = json.loads(response.contents)
-        if json_data.get('status') != 'success':
-            error_message = json_data.get('message')
-    else:
-        error_message = 'Invalid response'
+    try:
+        response = requests.get(url, params=params, auth=auth, verify=ssl_verify)
+        if (response.status_code == 200 and
+           response.headers.get('content_type') == 'application/json'):
+            json_data = json.loads(response.contents)
+            if json_data.get('status') != 'success':
+                error_message = json_data.get('message')
+        else:
+            error_message = 'Invalid response'
+
+    except requests.ConnectionError:
+        error_message = 'BDR endpoint was unreachable.'
 
     if error_message:
         current_app.logger.warning(error_message)
